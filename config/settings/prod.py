cf71b0ecef78fab135aa98ad42d9e5ebfd072f77
정상지 <sjung15@kr.kpmg.com>
2022-07-28 15:50:44 +0900

    도메인 등록

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['3.37.160.247']
+ALLOWED_HOSTS = ['3.37.160.247', 'accountant-sj.kr']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
