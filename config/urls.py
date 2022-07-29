dbb0f448dc4efa470967ac83aa4924ffc022da64
정상지 <sjung15@kr.kpmg.com>
2022-07-29 13:59:07 +0900

    URL 수정

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
+    path('', include('pybo.urls')),
     path('admin/', admin.site.urls),
     path('pybo/', include('pybo.urls')),
 ]
